57f69ef2f8d4d5d2eb30dcb9b27532bae3914d15
KuntaMallikRaj <kuntamallikraj@gmail.com>
2024-07-26 11:59:09 +0530

    fix average calculator

--- average-calculator/app.py
+++ average-calculator/app.py
@@ -2,17 +2,19 @@ import requests
 import asyncio
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
+import os
+from dotenv import load_dotenv
 
 app = FastAPI()
+
+load_dotenv()
 
 window = []
 window_size = 10
 
-access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzIxOTc0ODQzLCJpYXQiOjE3MjE5NzQ1NDMsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImYxOTYyNjU2LTY1MjktNGVmMi04NjAyLTM3MzIwM2MwMzZmZiIsInN1YiI6Imt1bnRhbWFsbGlrcmFqQGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6ImhleHRyZW5kcyIsImNsaWVudElEIjoiZjE5NjI2NTYtNjUyOS00ZWYyLTg2MDItMzczMjAzYzAzNmZmIiwiY2xpZW50U2VjcmV0IjoiZVRodnR6aUxSSW9McG9FUSIsIm93bmVyTmFtZSI6Ikt1bnRhIE1hbGxpayBSYWoiLCJvd25lckVtYWlsIjoia3VudGFtYWxsaWtyYWpAZ21haWwuY29tIiwicm9sbE5vIjoiMjE4OTFBMTIzNCJ9.nfWIS6FyKaZg3C6w6Llmm9OICvewNjo1-6zKJnqDyZs"
-
 async def fetch_data(url):
     try:
-        headers = {"Authorization": f"Bearer {access_token}"}
+        headers = {"Authorization": f"Bearer {os.environ.get("ACCESS_TOKEN")}"}
         async with asyncio.timeout(0.5):
             response = await asyncio.to_thread(requests.get, url, headers=headers)
             response.raise_for_status()
@@ -56,5 +58,5 @@ async def get_numbers(number_id: str):
     return JSONResponse({
         "initial_window": window,
         "current_window": window,
-        "average": 0
+        "average": average 
     })
